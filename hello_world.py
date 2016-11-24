047277846587648a55916cff200dc18b29e4c0b3
Axel Rovillé <axel.roville@gmail.com>
2016-11-24 11:17:38 +0100

    cleaning

--- hello_world.py
+++ hello_world.py
@@ -1,21 +1,30 @@
 import requests
-import xml
 import xml.etree.ElementTree as ET
+import multiprocessing
 
 url_base = 'http://ws.seloger.com/search.xml?'
 
-options = {
-    'idtypebien': '2,1',
-    'pxmax': '500000',
-    'idtt': '2',
-    'cp': '75',
-    'tri': 'initial'
-}
 
-params = '&'.join([k + "=" + v for k, v in options.items()])
-xml_file = requests.get(url_base + params)
-open('resp.xml', 'w+').write(xml_file.text)
-tree = ET.parse('resp.xml')
-root = tree.getroot()
-for prix in root.iter('prix'):
-    print(prix)
+def get_data(cp, type_bien):
+    options = {
+        'idtypebien': str(type_bien),
+        'pxmax': '500000',
+        'idtt': '2',
+        'cp': str(cp).zfill(2)
+    }
+
+    params = '&'.join([k + "=" + v for k, v in options.items()])
+    xml_file = requests.get(url_base + params)
+    open('resp.xml', 'w+').write(xml_file.text)
+    tree = ET.parse('resp.xml')
+    root = tree.getroot()
+    for prix in root.iter('prix'):
+        print(prix.text)
+
+for type_bien in [1, 2]:
+    for cp in range(1, 96):
+
+pool.map(get_data,
+    [cp, type_bien]
+    for type_bien in [1,2]
+    for cp in range(1, 96))
--- seloger.py
+++ seloger.py
@@ -2,7 +2,7 @@ import requests
 from lxml import etree
 import pandas as pd
 
-baseurl = "http://ws.seloger.com/search.xml?tri=initial&idtypebien=2,1&pxmax=5000&idtt=1&cp=75"
+baseurl = "http://ws.seloger.com/search.xml?"
 
 results = pd.DataFrame(columns=["cp", "pays", "ville", "surface", "nbPiece",
                                 "idTypeBien", "nbChambre", "prix"])
@@ -14,26 +14,30 @@ def save_xml(url, n):
     open("xml" + str(n), "w+").write(xml_file.text)
 
 def search(idtypebien, idtt, cp):
-    url = "http://ws.seloger.com/search.xml?tri=initial&idtypebien=" + \
-            str(idtypebien) + "&pxmax=2000000&idtt=" + str(idtt) + "&cp=" + \
-            str(75)
-    current = 1
+    options = {
+        'idtypebien': str(idtypebien),
+        'pxmax': '500000',
+        'idtt': str(idtt),
+        'cp': str(cp).zfill(2)
+    }
+    params = '&'.join([k + "=" + v for k, v in options.items()])
+
+    url = baseurl + params
+    new_url = url
+
     while True:
-        if current == 1:
-            new_url = url
         save_xml(new_url, current)
         tree = etree.parse(new_url)
         cur_line = results.shape[0]
         for field in results.columns:
             line = cur_line
             for s in tree.xpath("/recherche/annonces/annonce/" + field):
-                results.set_value(line, field, s.text)
+                results.set_value(line, field, s.text if s is not None else 'NA')
                 line += 1
         new_url_tree = tree.xpath("/recherche/pageSuivante")
-        if len(new_url_tree) ==  0:
+        if len(new_url_tree) == 0:
             break
         new_url = new_url_tree[0].text
-        current += 1
     return results
 
 

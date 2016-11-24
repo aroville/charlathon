dfd3f9c9efebbd0936fca36f911c1774ad28fa3d
Axel Rovillé <axel.roville@gmail.com>
2016-11-24 12:02:28 +0100

    saving whole df

--- seloger.py
+++ seloger.py
@@ -3,22 +3,20 @@ from lxml import etree
 import pandas as pd
 from multiprocessing import Pool
 
-baseurl = "http://ws.seloger.com/search.xml?"
+baseurl = 'http://ws.seloger.com/search.xml?'
+cols = ["cp", "pays", "ville", "surface", "nbPiece", "idTypeBien",
+        "nbChambre", "prix"]
 
-results = pd.DataFrame(columns=["cp", "pays", "ville", "surface", "nbPiece",
-                                "idTypeBien", "nbChambre", "prix"])
-
-line = 0
-
-def save_xml(url, n):
-    xml_file = requests.get(url)
-    open("xml" + str(n), "w+").write(xml_file.text)
 
 def search(args):
+    idtypebien = str(args[0])
+    idtt = str(args[1])
+    cp = str(args[2]).zfill(2)
+
     options = {
-        'idtypebien': str(args[0]),
-        'idtt': str(args[1]),
-        'cp': str(args[2]).zfill(2)
+        'idtypebien': idtypebien,
+        'idtt': idtt,
+        'cp': cp
     }
 
     params = '&'.join([k + "=" + v for k, v in options.items()])
@@ -27,17 +25,17 @@ def search(args):
     print(url)
     new_url = url
     current = 1
+    results = pd.DataFrame(columns=cols)
 
     while True:
-        save_xml(new_url, current)
         tree = etree.parse(new_url)
         cur_line = results.shape[0]
 
         for field in results.columns:
-            line = cur_line
+            l = cur_line
             for s in tree.xpath("/recherche/annonces/annonce/" + field):
-                results.set_value(line, field, s.text if s is not None else 'NA')
-                line += 1
+                results.set_value(l, field, s.text if s is not None else 'NA')
+                l += 1
         new_url_tree = tree.xpath("/recherche/pageSuivante")
         if len(new_url_tree) == 0:
             break
@@ -45,10 +43,17 @@ def search(args):
         current += 1
     return results
 
-pool = Pool(5)
-pool.map(search, [
+pool = Pool()
+results_tot = pool.map(search, [
     [idtypebien, idtt, cp]
-    for idtypebien in [1, 2, ]
-    for idtt in [1] #, 2, 4, 6, 7, 8, 9]
-    for cp in range(1, 2) #, 96)
+    for idtypebien in [1, 2, 4, 6, 7, 8, 9]
+    for idtt in [1, 2]
+    for cp in range(1, 96)
     ])
+
+
+df_tot = pd.DataFrame(results_tot[0])
+for df in results_tot[1:]:
+    df_tot.append(df)
+
+df_tot.to_csv('res.csv')

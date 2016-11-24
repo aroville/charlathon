6265fffc7a9d1b293ba6b03f37924a3b3a3d75f9
ngallot <nicolas.gallot@gmail.com>
2016-11-24 12:29:57 +0100

    Update AirBnb.py

--- AirBnb.py
+++ AirBnb.py
@@ -22,24 +22,31 @@ data_auth = {
 # Query API
 location = 'France'
 # https://api.airbnb.com/v2/search_results?client_id=3092nxybyb0otqw18e8nh5nty&locale=en-US&currency=USD&_format=for_search_results_with_minimal_pricing&_limit=10&_offset=0&fetch_facets=true&guests=1&ib=false&ib_add_photo_flow=true&location=Lake%20Tahoe%2C%20CA%2C%20US&min_bathrooms=0&min_bedrooms=0&min_beds=1&min_num_pic_urls=10&price_max=210&price_min=40&sort=1&user_lat=37.3398634&user_lng=-122.0455164
-data_get = {
-    'client_id': CLIENT_ID,
-    'locale': 'fr-FR',
-    'currency': 'EUR',
-    '_format': 'for_search_results_with_minimal_pricing',
-    'ib_add_photo_flow': 'false',
-    'location': location,
-    '_limit': 50,
-    '_offset': 50
-}
+
+def get_request_params(offset):
+    return {
+        'client_id': CLIENT_ID,
+        'locale': 'fr-FR',
+        'currency': 'EUR',
+        '_format': 'for_search_results_with_minimal_pricing',
+        'ib_add_photo_flow': 'false',
+        'location': location,
+        '_limit': 50,
+        '_offset': offset
+        }
 
 listings = list()
+offset = 0
+limit = 50
+
 for i in range(0, 2):
-    r = requests.get(URL_SEARCH, params=data_get)
+    params = get_request_params(offset)
+    r = requests.get(URL_SEARCH, params=params)
     print(r.url)
     res = json.loads(r.text)
     listing = res["search_results"]
     listings.append(listing)
+    offset += limit
     # listings = listings + listing
 
 
